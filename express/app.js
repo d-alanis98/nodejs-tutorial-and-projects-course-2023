9772f3b9c079bd6bbdf9e7299184220ddbd86582
Damian Alanis <damian.alanis98@gmail.com>
2023-01-15 02:09:20 -0600

    API example with express

--- express/app.js
+++ express/app.js
@@ -1,12 +1,37 @@
+const path = require('path');
 const express = require('express');
+// Data
+const products = require('./data/products');
 
-const app = express();
-
+// Constants
 const PORT = process.env.PORT ?? 8000;
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Welcome</h1>`);
+const app = express();
+// Set up middlewares
+app.use(express.static('./public'));
+
+
+app.get('/api/v1', (req, res) => {
+   return res.json(products);
 });
 
+app.get('/api/v1/query', (req, res) => {
+    let { search, limit } = req.query;
+    let resultProducts = products;
+    if(search)
+        resultProducts = products.filter(product => product.name.toLowerCase().startsWith(search.toLowerCase()));
+
+    if(limit !== undefined)
+        resultProducts = resultProducts.slice(0, Number.parseInt(limit));
+    
+    if(resultProducts.length === 0)
+        return res.status(404).send('No products matched your search');
+
+    return res.json(resultProducts);
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resource not found</h1>');
+});
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${ PORT }`));
---
+++ express/data/products.js
@@ -0,0 +1,22 @@
+module.exports = [
+    { 
+        id: 1,
+        name: 'Keyboard',
+        price: 120
+    },
+    {
+        id: 2,
+        name: 'Mouse',
+        price: 80
+    },
+    {
+        id: 3,
+        name: 'CPU',
+        price: 500
+    },
+    {
+        id: 4,
+        name: 'Smartphone',
+        price: 600
+    }
+]
