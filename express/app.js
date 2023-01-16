548631139bb9f84a7b43a162f4d8b49e25d7bd91
Damian Alanis <damian.alanis98@gmail.com>
2023-01-16 01:25:55 -0600

    Add pug templates

--- express/app.js
+++ express/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 // Middlewares
 const logger = require('./middleware/logger');
@@ -13,15 +14,27 @@ const app = express();
 // Serve static content
 app.use(express.static('./public'));
 // Morgan (third-party middleware for logging)
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 // Parse form data
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // Parse JSON
-app.use(express.json())
+app.use(express.json());
+
+
+// Configurations
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+
 
 // Custom middlewares
 // Apply logger middleware to API routes
 app.use('/api/v1', logger, apiRoutes);
+
+app.get('/pug', (req, res) => {
+    let { name = 'John Doe', age = 24 } = req.query;
+    return res.render('index.pug', { name, age });
+})
 
 app.all('*', (req, res) => {
     res.status(404).send('<h1>Resource not found</h1>');
